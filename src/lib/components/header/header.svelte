ba2f4ceea015a485f0a25e83b8c0d1fa6b4ebf6c
8to16 <ampelectrecuted@gmail.com>
2025-11-06 14:54:28 +0000

    feat: change size of tw-advanced icon, add hover animation

--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -1,5 +1,5 @@
 <!-- TODO: Add stuff for the website. This file is modified from website-v2 in ampmod/extensions.
-     Styling is inspired by the AmpMod static website UI. -->
+      Styling is inspired by the AmpMod static website UI. -->
 <script lang="ts">
   import Logo from './tw-advanced.svelte';
 </script>
@@ -8,7 +8,10 @@
   class="flex h-12 w-full items-center border-b border-black/10 bg-white px-2 font-sans text-sm text-black md:px-4 dark:bg-accent-secondary dark:text-white"
 >
   <div class="flex w-full items-center justify-center gap-4">
-    <a href="/" class="logo-link flex items-center">
+    <a
+      href="/"
+      class="logo-link flex items-center transition-transform duration-250 hover:scale-115 hover:rotate-360"
+    >
       <Logo />
     </a>
 
