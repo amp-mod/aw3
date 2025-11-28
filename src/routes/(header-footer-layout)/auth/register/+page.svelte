f51c096b01f9e4a041241a18da2309a2e435b813
8to16 <ampelectrecuted@gmail.com>
2025-11-28 23:57:17 +0000

    feat: upgraded authentication * Add CAPTCHA. * Separate register and login pages. * Allow logging out via the header.

---
+++ src/routes/(header-footer-layout)/auth/register/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { enhance } from '$app/forms';
+  import type { ActionData } from './$types';
+  import { browser } from '$app/environment';
+  let loading = $state(true);
+  if (browser) {
+    import('altcha');
+    loading = false;
+  }
+
+  let { form }: { form: ActionData } = $props();
+</script>
+
+<form
+  method="post"
+  action="?/register"
+  use:enhance
+  class="m-auto flex max-w-2xl flex-col gap-3 px-2 py-8"
+>
+  <h1 class="text-2xl">Join AmpMod</h1>
+  <label>
+    Username
+    <input name="username" class="input" required />
+  </label>
+  <label>
+    Password
+    <input type="password" name="password" class="input" required />
+  </label>
+  <label>
+    Confirm password
+    <input type="password" name="password2" class="input" required />
+  </label>
+  {#if !loading}<altcha-widget auto="onfocus" challengeurl="/auth/_altcha" hidden
+    ></altcha-widget>{/if}
+  <button class="btn" disabled={loading}>Register</button>
+  <p class="text-red-500">{form?.message ?? ''}</p>
+</form>
