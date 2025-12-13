e086f1883c3bc2fa497fae420b8c474a3132cca8
8to16 <ampelectrecuted@gmail.com>
2025-12-13 22:47:51 +0000

    feat: add Scratch proxies, warehouse is a placeholder

---
+++ src/routes/catvet/[...apiroute]/+server.ts
@@ -0,0 +1,32 @@
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async ({ url }) => {
+  console.log(url);
+  if (url.method !== 'GET') {
+    return new Response('Method Not Allowed', { status: 405 });
+  }
+
+  const apiRoute = url.pathname.replace(/^\/catvet\/?/, '');
+
+  const apiUrl = `https://api.scratch.mit.edu/${apiRoute}`;
+
+  try {
+    const res = await fetch(apiUrl, {
+      headers: {
+        'User-Agent': 'curl/8.17.0 aw3 catvet https://codeberg.org/ampmod/aw3'
+      }
+    });
+
+    const contentType = res.headers.get('content-type') ?? 'application/octet-stream';
+
+    return new Response(res.body, {
+      headers: {
+        'Content-Type': contentType,
+        Server: 'aw3 catvet'
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    return new Response('', { status: 500 });
+  }
+};
