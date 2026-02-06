59dbe7cf9d1d85c8fcc1e0773f339f00c27fba02
8to16 <ampelectrecuted@gmail.com>
2026-02-06 12:28:37 +0000

    feat: majorly update the db schema this is to allow us to add a few new features

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,21 +1,43 @@
-import { pgTable, smallint, text, timestamp, varchar } from 'drizzle-orm/pg-core'
+import { pgTable, smallint, text, timestamp, varchar, index, boolean } from 'drizzle-orm/pg-core'
 
-export const user = pgTable('user', {
-	id: text('id').primaryKey(),
-	username: text('username').notNull().unique(),
-	passwordHash: text('password_hash').notNull(),
-	bio: varchar({ length: 256 }).default(''),
-	rank: smallint('rank').default(0),
-})
+export const user = pgTable(
+	'user',
+	{
+		id: text('id').primaryKey(),
+		username: text('username').notNull().unique(),
+		passwordHash: text('password_hash').notNull(),
+		bio: varchar({ length: 256 }).default(''),
+		/*
+         0 = new ampmodder
+         1 = ampmodder
+         2 = moderator
+         3 = operator
 
-export const session = pgTable('session', {
-	id: text('id').primaryKey(),
-	userId: text('user_id')
-		.notNull()
-		.references(() => user.id),
-	expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull(),
-})
+         we most likely won't expand the number of ranks anytime soon,
+         which is why this is a smallint
+      */
+		rank: smallint('rank').default(0),
+		createdAt: timestamp('created_at', { withTimezone: true, mode: 'date' }).notNull().defaultNow(),
+		// welcome to the banlands
+		banned: boolean('banned').default(false),
+		bannedExpiry: timestamp('banned_expiry', { withTimezone: true, mode: 'date' })
+			.notNull()
+			.defaultNow(),
+	},
+	(table) => [index('username_idx').on(table.username)],
+)
+
+export const session = pgTable(
+	'session',
+	{
+		id: text('id').primaryKey(),
+		userId: text('user_id')
+			.notNull()
+			.references(() => user.id, { onDelete: 'cascade' }),
+		expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull(),
+	},
+	(table) => [index('user_id_idx').on(table.userId)],
+)
 
 export type Session = typeof session.$inferSelect
-
 export type User = typeof user.$inferSelect
