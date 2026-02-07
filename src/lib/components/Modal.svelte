f23f8cbc4c012072810ca58451899d8cde10aeea
8to16 <ampelectrecuted@gmail.com>
2026-02-07 14:47:38 +0000

    feat: fix dark mode modal shadow looking ugly

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -12,7 +12,7 @@
 		/>
 
 		<Dialog.Content
-			class="fixed top-[120px] left-[50%] z-50 w-full max-w-[600px] translate-x-[-50%] overflow-hidden rounded-lg bg-white shadow-[0px_0px_0px_4px_rgba(0,0,0,0.1)] outline-none dark:bg-neutral-900 dark:shadow-[0px_0px_0px_4px_rgba(100,100,100,0.5)]"
+			class="fixed top-[120px] left-[50%] z-50 w-full max-w-[600px] translate-x-[-50%] overflow-hidden rounded-lg bg-white shadow-[0px_0px_0px_4px_rgba(0,0,0,0.1)] outline-none dark:bg-neutral-900 dark:shadow-[0px_0px_0px_4px_rgba(255,255,255,0.1)]"
 		>
 			<div
 				class="relative flex h-12 items-center justify-between border-b border-b-black/10 px-4 dark:border-b-transparent dark:bg-accent"
