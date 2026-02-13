3afdcf3061466c119df802e31c14684c428890ef
8to16 <ampelectrecuted@gmail.com>
2026-02-13 14:14:43 +0000

    feat: redesign footer

--- src/lib/components/footer/footer.svelte
+++ src/lib/components/footer/footer.svelte
@@ -1,50 +1,38 @@
-<script>
-	import SiScratch from '~icons/simple-icons/scratch'
-	import SiCodeberg from '~icons/simple-icons/codeberg'
-	import SiMiraheze from '~icons/simple-icons/miraheze'
-	import SiYoutube from '~icons/simple-icons/youtube'
-</script>
+<footer class="bg-neutral-150 w-full bg-neutral-200 px-4 py-4 dark:bg-neutral-800">
+	<div class="mx-auto max-w-6xl">
+		<div class="mb-12 grid grid-cols-2 gap-8 text-sm md:grid-cols-4">
+			<div class="flex flex-col space-y-2">
+				<span class="mb-1 font-bold">About</span>
+				<a href="/about" class="link">About AmpMod</a>
+				<a href="/credits" class="link">Credits</a>
+			</div>
 
-<footer
-	class="dark:black/10 flex flex-col items-center justify-center space-y-2 border-t border-black/10 py-8 text-neutral-500 dark:border-white/10 dark:text-neutral-300"
->
-	<div class="flex items-center justify-center space-x-6">
-		<!-- <a
-      href="https://scratch.mit.edu/discuss/topic/806311/"
-      class="hover:text-[#855CD6] focus:ring-2 focus:ring-[#855CD6] focus:ring-offset-2 focus:outline-none"
-      ><SiScratch size={32} title="Scratch" /></a
-    > *-->
-		<a
-			href="https://codeberg.org/ampmod"
-			class="text-2xl hover:text-[#2185D0] focus:ring-2 focus:ring-[#2185D0] focus:ring-offset-2 focus:outline-none"
-			><SiCodeberg size={32} title="Codeberg" /></a
-		>
-		<a
-			href="https://ampmod.miraheze.org"
-			class="text-2xl hover:text-yellow-500 focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 focus:outline-none"
-			><SiMiraheze size={32} title="Wiki (Miraheze)" /></a
-		>
-		<a
-			href="https://youtube.com/@Amp-Mod"
-			class="text-2xl hover:text-[#FF0033] focus:ring-2 focus:ring-[#FF0033] focus:ring-offset-2 focus:outline-none"
-			><SiYoutube size={32} title="YouTube" /></a
-		>
+			<div class="flex flex-col space-y-2">
+				<span class="mb-1 font-bold">Community</span>
+				<a href="/projects/explore" class="link">Explore projects</a>
+				<a href="https://ampblog.flarum.cloud" class="link">AmpMod Blog</a>
+				<a href="https://ampmod.miraheze.org" class="link">AmpMod Wiki</a>
+				<a href="https://youtube.com/@Amp-Mod" class="link">YouTube</a>
+			</div>
+
+			<div class="flex flex-col space-y-2">
+				<span class="mb-1 font-bold">Resources</span>
+				<a href="/source-code" class="link">Source Code</a>
+				<a href="/faq" class="link">FAQ</a>
+			</div>
+
+			<div class="flex flex-col space-y-2">
+				<span class="mb-1 font-bold">Policy</span>
+				<a href="/guidelines" class="link">Guidelines</a>
+				<a href="/terms" class="link">Terms of Service</a>
+				<a href="/privacy" class="link">Privacy Policy</a>
+			</div>
+		</div>
+
+		<div class="flex flex-col items-center space-y-2">
+			<p class="text-center text-xs opacity-75">
+				AmpMod is not affiliated with Scratch or TurboWarp.
+			</p>
+		</div>
 	</div>
-
-	<div class="mt-2 flex space-x-4 text-sm">
-		<a href="/credits" class="underline focus:ring-2 focus:ring-offset-2 focus:outline-none"
-			>Credits</a
-		>
-		<a href="/faq" class="underline focus:ring-2 focus:ring-offset-2 focus:outline-none">FAQ</a>
-		<a href="/tips" class="underline focus:ring-2 focus:ring-offset-2 focus:outline-none">Tips</a>
-		<a href="/terms" class="underline focus:ring-2 focus:ring-offset-2 focus:outline-none">Terms</a>
-		<a href="/privacy" class="underline focus:ring-2 focus:ring-offset-2 focus:outline-none"
-			>Privacy</a
-		>
-		<a href="/source-code" class="underline focus:ring-2 focus:ring-offset-2 focus:outline-none"
-			>Source code</a
-		>
-	</div>
-
-	<span class="mt-2 text-center text-sm">Not affiliated with Scratch or TurboWarp.</span>
 </footer>
--- src/routes/(header-footer-layout)/+layout.svelte
+++ src/routes/(header-footer-layout)/+layout.svelte
@@ -7,22 +7,26 @@
 
 <div class="flex h-screen flex-col">
 	<Header {data} />
-	<div class="flex-1 overflow-auto shadow-inner" id="main">
-		<main>
-			{#if data.isDangerousMode}
-				<div class="bg-red-700 p-2 text-center text-lg font-bold">
-					Your admin panel is currently open to the world wide web, meaning anyone can access it,
-					even maliciously. Remove the AW3_FORCE_ADMIN env var to remove this banner.
-				</div>
-			{/if}
-			<noscript>
-				<div class="bg-red-700 p-2 text-center text-lg font-bold">
-					Enable JavaScript to use most features on AmpMod, including logging in or running
-					projects. If you're afraid of remote scripts, download AmpMod on your computer.
-				</div>
-			</noscript>
-			{@render children?.()}
+
+	<div class="flex flex-1 flex-col overflow-auto shadow-inner">
+		<main class="flex flex-1 flex-col">
+			<div>
+				{#if data.isDangerousMode}
+					<div class="bg-red-700 p-2 text-center text-lg font-bold">
+						Your admin panel is currently open to the world wide web, meaning anyone can access it,
+						even maliciously. Remove the AW3_FORCE_ADMIN env var to remove this banner.
+					</div>
+				{/if}
+				<noscript>
+					<div class="bg-red-700 p-2 text-center text-lg font-bold">
+						Enable JavaScript to use most features on AmpMod, including logging in or running
+						projects. If you're afraid of remote scripts, download AmpMod on your computer.
+					</div>
+				</noscript>
+				{@render children?.()}
+			</div>
 		</main>
+
 		<Footer />
 	</div>
 </div>
