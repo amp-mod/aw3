a6f116f49d786ef1a487c238e3be29ce0e4b1893
8to16 <ampelectrecuted@gmail.com>
2026-02-13 17:05:11 +0000

    feat: RTL support

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,10 @@
 	import { modals } from '$lib/modals.svelte'
 	import favicon from '$lib/assets/favicon.ico'
 	import LoginModal from '$lib/components/LoginModal.svelte'
+	import { getLocale } from '$lib/paraglide/runtime'
 
 	let { children, data } = $props()
+	const dir = ['ar', 'he'].includes(getLocale()) ? 'rtl' : 'ltr'
 </script>
 
 <svelte:head>
@@ -12,14 +14,16 @@
 	<title>AmpMod</title>
 </svelte:head>
 
-{@render children?.()}
+<div {dir}>
+	{@render children?.()}
 
-<LoginModal bind:open={modals.login} />
+	<LoginModal bind:open={modals.login} />
 
-{#if data?.isDangerousMode}
-	<div
-		class="pointer-events-none fixed right-4 bottom-4 z-999999 text-xl font-bold text-red-500 opacity-50"
-	>
-		Insecure
-	</div>
-{/if}
+	{#if data?.isDangerousMode}
+		<div
+			class="pointer-events-none fixed right-4 bottom-4 z-999999 text-xl font-bold text-red-500 opacity-50"
+		>
+			Insecure
+		</div>
+	{/if}
+</div>
