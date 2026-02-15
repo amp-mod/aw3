266a003501d7c1d44101f085729ab063db7d8c37
8to16 <ampelectrecuted@gmail.com>
2026-02-15 15:03:13 +0000

    feat: vendor scaffolding

--- eslint.config.js
+++ eslint.config.js
@@ -12,9 +12,11 @@ import ts from 'typescript-eslint'
 import svelteConfig from './svelte.config.js'
 
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url))
+const eslintignorePath = fileURLToPath(new URL('./.eslintignore', import.meta.url))
 
 export default defineConfig(
 	includeIgnoreFile(gitignorePath),
+	includeIgnoreFile(eslintignorePath),
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs.recommended,
