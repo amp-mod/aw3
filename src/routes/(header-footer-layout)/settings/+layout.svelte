6dab243eb1895d26aefb026cd291f1cc175276dc
8to16 <ampelectrecuted@gmail.com>
2026-02-22 23:05:09 +0000

    feat: redesign settings layout

--- src/routes/(header-footer-layout)/settings/+layout.svelte
+++ src/routes/(header-footer-layout)/settings/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-	import { ExternalLink } from '@lucide/svelte'
+	import { ExternalLink, UserRound, Palette, Key, Puzzle, Paintbrush } from '@lucide/svelte'
 	import { page } from '$app/state'
 	import LoginModal from '$lib/components/LoginModal.svelte'
+	import { m } from '$lib/paraglide/messages.js'
 
 	const { data, children } = $props()
 
@@ -11,25 +12,37 @@
 		href?: string
 		externalIcon?: boolean
 		restricted?: boolean
+		icon?: any
 	}
 
 	const tabs: Tab[] = [
 		{ label: 'Account', restricted: true },
-		{ id: 'profile', label: 'Profile', href: '/settings/profile', restricted: true },
-		{ id: 'sessions', label: 'Sessions', href: '/settings/session', restricted: true },
-		{ id: 'danger', label: 'Dangerous', href: '/settings/danger', restricted: true },
-		{ label: 'Editor' },
-		{ id: 'theme', label: 'Theme', href: '/settings/theme' },
+		{
+			id: 'profile',
+			label: 'Public profile',
+			href: '/settings/profile',
+			restricted: true,
+			icon: UserRound,
+		},
+		{
+			id: 'sessions',
+			label: 'Sessions',
+			href: '/settings/session',
+			restricted: true,
+			icon: Key,
+		},
+		{ label: 'AmpMod editor' },
+		{ id: 'theme', label: 'Theme', href: '/settings/theme', icon: Paintbrush },
 		{
 			id: 'addons',
 			label: 'Addons',
 			href: '/settings/addons',
 			externalIcon: true,
+			icon: Puzzle,
 		},
 	]
 
 	const activeTab = $derived(tabs.find((t) => t.href === page.url.pathname))
-
 	const activeTabLabel = $derived(activeTab?.label ?? 'Settings')
 	const isRestrictedPath = $derived(!!activeTab?.restricted)
 </script>
@@ -42,46 +55,47 @@
 	<title>Settings: {activeTabLabel} - AmpMod</title>
 </svelte:head>
 
-<h1 class="mx-auto mt-8 mb-4 max-w-5xl text-3xl font-bold text-black dark:text-white">Settings</h1>
+<div class="mx-auto my-12 mb-24 flex min-h-120 max-w-5xl gap-3 overflow-hidden">
+	<nav class="flex w-64 flex-col gap-1">
+		<h1 class="mb-4 text-2xl font-bold text-black dark:text-white">{m.settings()}</h1>
 
-<div
-	class="mx-auto mb-24 flex min-h-120 max-w-5xl overflow-hidden rounded-xl border border-black/10 bg-neutral-100 dark:border-white/10 dark:bg-neutral-800"
->
-	<nav class="flex w-64 flex-col gap-2 bg-neutral-200 p-4 dark:bg-accent-secondary">
 		{#each tabs as tab}
 			{#if !tab.id && (data.user || !tab.restricted)}
-				<div class="flex w-full items-center gap-3 py-2">
-					<div class="text-sm font-bold">
-						{tab.label}
-					</div>
-					<div class="flex-1 border-t border-neutral-500/30 dark:border-accent-tertiary"></div>
+				<div
+					class="flex w-full items-center border-b border-neutral-500/30 pt-3 pb-1 text-sm font-bold text-neutral-500"
+				>
+					{tab.label}
 				</div>
 			{:else if data.user || !tab.restricted}
 				<a
 					id={tab.id}
-					class="rounded px-3 py-2 text-left text-black transition-colors hover:bg-black/10 dark:text-white dark:hover:bg-accent-tertiary
+					class="group flex items-center justify-between rounded-md border-l-3 border-transparent px-3 py-2 text-sm text-black hover:bg-black/5 dark:text-white dark:hover:bg-white/5
                {page.url.pathname === tab.href
-						? 'bg-black/10 font-semibold dark:bg-accent-tertiary'
+						? 'border-accent bg-black/10 font-semibold dark:border-accent-light dark:bg-white/10'
 						: ''}"
 					href={tab.href}
 					target={tab.externalIcon ? '_blank' : undefined}
 					aria-current={page.url.pathname === tab.href ? 'page' : undefined}
 				>
-					<div class="flex items-center justify-between">
-						<div class="inline-flex items-center gap-2">
-							{tab.label}
-						</div>
-						{#if tab.externalIcon}
-							<ExternalLink size={18} class="text-black dark:text-white" />
+					<div class="flex items-center gap-3">
+						{#if tab.icon}
+							<tab.icon size={18} strokeWidth={page.url.pathname === tab.href ? 2.5 : 2} />
 						{/if}
+						<span>{tab.label}</span>
 					</div>
+
+					{#if tab.externalIcon}
+						<ExternalLink size={18} />
+					{/if}
 				</a>
 			{/if}
 		{/each}
 	</nav>
 
+	<div class="border-r border-r-neutral-500/10"></div>
+
 	{#if data.user || !isRestrictedPath}
-		<div class="flex-1 p-6 text-black dark:text-white">
+		<div class="flex-1 px-4">
 			{@render children()}
 		</div>
 	{/if}
