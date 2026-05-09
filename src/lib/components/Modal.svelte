5c3739e78f3eaa0af9179c8c9dd22a73ab7f3cea
amp <ampelectrecuted@gmail.com>
2026-05-09 15:57:25 +0100

    feat: redesign modals

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -7,24 +7,19 @@
 
 <Dialog.Root bind:open>
 	<Dialog.Portal>
-		<Dialog.Overlay
-			class="fixed inset-0 z-50 bg-accent/70 backdrop-blur-[2px] dark:bg-neutral-500/40"
-			{forceMount}
-		/>
+		<Dialog.Overlay class="fixed inset-0 z-50 bg-black/70 backdrop-blur-[2px]" {forceMount} />
 
 		<Dialog.Content
-			class="not-xl:h-[calc(100% - 12px)] fixed top-[50%] left-[50%] z-50 w-full max-w-150 translate-x-[-50%] overflow-auto rounded bg-white shadow-[0px_0px_0px_4px_rgba(0,0,0,0.05)] outline-none not-xl:translate-x-[-50%] not-xl:translate-y-[-50%] xl:top-20 dark:bg-neutral-900 dark:shadow-[0px_0px_0px_4px_rgba(255,255,255,0.1)]"
+			class="not-xl:h-[calc(100% - 12px)] fixed top-[50%] left-[50%] z-50 w-full max-w-150 translate-x-[-50%] overflow-auto rounded-xl bg-white shadow-[0px_0px_0px_4px_rgba(0,0,0,0.05)] outline-none not-xl:translate-x-[-50%] not-xl:translate-y-[-50%] xl:top-20 dark:bg-neutral-900 dark:shadow-[0px_0px_0px_4px_rgba(255,255,255,0.1)]"
 			escapeKeydownBehavior={canClose ? 'close' : 'ignore'}
 			interactOutsideBehavior={canClose ? 'close' : 'ignore'}
 			onOpenAutoFocus={(e) => e.preventDefault()}
 			{forceMount}
 		>
 			<div
-				class="relative flex h-12.5 items-center justify-between border-b border-b-black/10 px-4 dark:border-b-transparent dark:bg-accent"
+				class="relative flex h-10 items-center justify-between border-b border-b-black/10 px-4 dark:border-b-transparent dark:bg-accent"
 			>
-				<Dialog.Title
-					class="absolute left-1/2 -translate-x-1/2 font-semibold whitespace-nowrap text-neutral-800 dark:text-white"
-				>
+				<Dialog.Title class="font-semibold whitespace-nowrap text-neutral-800 dark:text-white">
 					{title}
 				</Dialog.Title>
 
