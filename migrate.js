50b0d3b2e0b8076375e926933d694d2f25f8327e
Amp <ampelectrecuted@gmail.com>
2026-09-16 17:25:03 +0100

    okay

--- migrate.js
+++ migrate.js
@@ -14,7 +14,7 @@ async function runMigrations() {
 	const db = drizzle(sql)
 
 	console.log('Running Drizzle migrations...')
-	await migrate(db, { migrationsFolder: './drizzle/migrations' })
+	await migrate(db, { migrationsFolder: './drizzle' })
 	console.log('Migrations complete!')
 
 	await sql.end()
